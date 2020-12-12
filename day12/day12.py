cd14a19579a91e9968a24a08c637030e546046a7
Lisaf Iluz <liluz@ebay.com>
2020-12-12 10:56:20 +0200

    untill day 12_5

--- day12/day12.py
+++ day12/day12.py
@@ -1,31 +1,34 @@
 def get_file_data(path):
-    # Last line without new line (\n)
     with open(path, 'r') as file_handler:
         lines = file_handler.readlines()
-    lines_without_spaces = [line[0:-1] for line in lines[0:-1]]
-    lines_without_spaces.append(lines[-1])
+    lines_without_spaces = [line.strip() for line in lines]
     return lines_without_spaces
+
+
+def get_current_degrees(current_degrees, value):
+    current_degrees += value
+    if current_degrees >= 360:
+        current_degrees -= 360
+    elif current_degrees < 0:
+        current_degrees += 360
+    return current_degrees
 
 
 def part1(rules):
     turns = {0: 'N', 90: 'E', 180: 'S', 270: 'W'}
     directions = {'N': 0, 'S': 0, 'W': 0, 'E': 0}
     current_direction = 'E'
-    current_degres = 90
+    current_degrees = 90
 
     for direction, value in rules:
         if direction == 'F':
             directions[current_direction] += value
         elif direction =='R':
-            current_degres += value
-            if current_degres >= 360:
-                current_degres -= 360
-            current_direction = turns[current_degres]    
+            current_degrees = get_current_degrees(current_degrees, value)
+            current_direction = turns[current_degrees] 
         elif direction == 'L':
-            current_degres -= value
-            if current_degres < 0:
-                current_degres += 360
-            current_direction = turns[current_degres]
+            current_degrees = get_current_degrees(current_degrees, value*-1)
+            current_direction = turns[current_degrees]
         else:
             directions[direction] += value
     return abs(directions['S'] - directions['N']) + abs(directions['E'] - directions['W'])
@@ -33,20 +36,13 @@ def part1(rules):
 
 def rotate_way_point(current_way_point, rotates, rotate_dir):
     new_way_point = {}
-    if rotate_dir:  # To the right
-        for _ in range(rotates):
-            new_way_point['E'] = current_way_point['N']
-            new_way_point['S'] = current_way_point['E']
-            new_way_point['W'] = current_way_point['S']
-            new_way_point['N'] = current_way_point['W']
-            current_way_point = new_way_point.copy()
-    else:  # To the left
-       for _ in range(rotates):
-            new_way_point['W'] = current_way_point['N']
-            new_way_point['S'] = current_way_point['W']
-            new_way_point['E'] = current_way_point['S']
-            new_way_point['N'] = current_way_point['E']
-            current_way_point = new_way_point.copy()
+    directions = 'NESW'
+    for _ in range(rotates):
+        new_way_point[directions[1*rotate_dir]] = current_way_point[directions[0*rotate_dir]]
+        new_way_point[directions[2*rotate_dir]] = current_way_point[directions[1*rotate_dir]]
+        new_way_point[directions[3*rotate_dir]] = current_way_point[directions[2*rotate_dir]]
+        new_way_point[directions[0*rotate_dir]] = current_way_point[directions[3*rotate_dir]]
+        current_way_point = new_way_point.copy()
     return new_way_point
 
 
@@ -59,9 +55,9 @@ def part2(rules):
             for k, _ in current_way_point.items():
                 location[k] += value * current_way_point[k]
         elif direction =='R':
-            current_way_point = rotate_way_point(current_way_point, value//90, True)
+            current_way_point = rotate_way_point(current_way_point, rotates=value//90, rotate_dir=1)
         elif direction == 'L':
-            current_way_point = rotate_way_point(current_way_point, value//90, False)
+            current_way_point = rotate_way_point(current_way_point, rotates=value//90, rotate_dir=-1)
         else:
             current_way_point[direction] += value
     return abs(location['S'] - location['N']) + abs(location['E'] - location['W'])
